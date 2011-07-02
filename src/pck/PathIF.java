591b438516c626b54d67075e6ccbfde0412612c8
Cory <Cory@172.17.108.93>
2011-07-01 22:49:02 -0500

    It findsthe nodes in the loop, closure is what i didn't want it to be... v.closure() is actually nodes reachable from v.. and not the weird multiplication way of doing it.. it seems atleast.

--- src/pck/PathIF.java
+++ src/pck/PathIF.java
@@ -14,17 +14,15 @@ public class PathIF {
 
 	private final Relation Edge, begin, end;
 
-	private final Relation Visit, ref, next, Start_Loop, End_Loop, srt_ptr, end_ptr;
+	private final Relation Visit, ref, next, start_loop;
 
 	public PathIF() {														/* Path */
 		Node = Relation.unary("Node");
 		Edge = Relation.unary("Edge");
 		Visit = Relation.unary("Visit");
-		Start_Loop = Relation.unary("Start_Loop");
-		End_Loop = Relation.unary("End_Loop");
+		start_loop = Relation.unary("start_loop");
 
-		srt_ptr = Relation.binary("srt_ptr");
-		end_ptr = Relation.binary("end_ptr");
+
 		begin = Relation.binary("begin");
 		end = Relation.binary("end");
 		ref = Relation.binary("ref");
@@ -39,9 +37,7 @@ public class PathIF {
 		final Formula f1 = end.function(Edge, Node);
 		final Formula f2 = ref.function(Visit, Edge);						/* Node */
 		final Formula f3 = next.partialFunction(Visit, Visit);
-		final Formula fdec = srt_ptr.function(Start_Loop, Node);
-		final Formula fdec2 = end_ptr.function(End_Loop, Node);
-		return f0.and(f1).and(f2).and(f3).and(fdec).and(fdec2);
+		return f0.and(f1).and(f2).and(f3);
 	}
 
 	public final Formula facts() {
@@ -49,8 +45,8 @@ public class PathIF {
 		final Variable w = Variable.unary("w");
 		final Variable e = Variable.unary("e");								/* n */
 		final Variable d = Variable.unary("d");								/* d */
-		final Variable srt = Variable.nary("srt", 2);
-		final Variable loo = Variable.unary("loo");
+		final Variable n = Variable.unary("n");
+		final Variable st = Variable.unary("start_loop");
 	//	final Variable end = Variable.unary("end");
 		/*	final Variable e = Variable.unary("e");	*/
 
@@ -80,17 +76,14 @@ public class PathIF {
 		final Formula f15 = f13.and(f14);
 		final Formula f16 = f15.forSome(v.oneOf(Visit)).forAll(w.oneOf(Visit));
 		
+		final Formula f17 = n.in(n.join(((begin.transpose()).join(end)).closure()));
+		final Formula f18 = n.in(start_loop);
+		final Formula f19 = f17.iff(f18);
+		final Formula f20 = f19.forAll((n.oneOf(Node)));
+		
 		
 		
 		// one srt: srt_ptr, one v:node, one d:start_loop | v in v.^(~begin.end) IFF srt = loo->v
-		
-		
-		final Formula f17 = v.in(v.join(((begin.transpose()).join(end)).closure())        );
-		final Formula f18 = ( (loo.product(v))).in(srt);
-		final Formula f19 = f17.iff(f18);
-		final Formula f20 = f19.forAll(loo.oneOf(Start_Loop)).forAll(v.oneOf(Node)).forAll(srt.oneOf(srt_ptr));
-		
-		
 		return f5.and(f8).and(f12).and(f16).and(f20);
 	}
 
@@ -102,16 +95,12 @@ public class PathIF {
 		assert scope > 0;
 		final int n = scope + 13;
 		final List<String> atoms = new ArrayList<String>(n);
-		for (int i = 1; i <= 6; i++)
+		for (int i = 1; i <= 4; i++)
 			atoms.add("Node" + i);
-		for (int i = 1; i <= 7; i++)
+		for (int i = 1; i <= 4; i++)
 			atoms.add("Edge" + i);
 		for (int i = 0; i < scope; i++)
 			atoms.add("Visit" + i);
-		for (int i = 0; i < 1; i++){
-			atoms.add("Start_Loop" + i);
-			atoms.add("End_Loop" + i);
-		}
 
 		final Universe u = new Universe(atoms);
 		final TupleFactory f = u.factory();
@@ -119,18 +108,14 @@ public class PathIF {
 
 		final int max = scope - 1;
 
-		b.bound(Node, f.range(f.tuple("Node1"), f.tuple("Node6")));				/* Java will not instantiate new Nodes. */
-		b.bound(Edge, f.range(f.tuple("Edge1"), f.tuple("Edge7")));				/* Java will not instantiate new Edges. */
+		b.bound(Node, f.range(f.tuple("Node1"), f.tuple("Node4")));				/* Java will not instantiate new Nodes. */
+		b.bound(Edge, f.range(f.tuple("Edge1"), f.tuple("Edge4")));				/* Java will not instantiate new Edges. */
 		b.bound(Visit, f.range(f.tuple("Visit0"), f.tuple("Visit" + max)));
-		b.bound(Start_Loop, f.range(f.tuple("Start_Loop0"), f.tuple("Start_Loop0")));
-		b.bound(End_Loop, f.range(f.tuple("End_Loop0"), f.tuple("End_Loop0")));
+		b.bound(start_loop, f.range(f.tuple("Node1"), f.tuple("Node4")));
 		
 		b.bound(ref, b.upperBound(Visit).product(b.upperBound(Edge)));		/* Node */
 		b.bound(next, b.upperBound(Visit).product(b.upperBound(Visit)));
 		
-		b.bound(srt_ptr, b.upperBound(Start_Loop).product(b.upperBound(Node)));
-		
-		b.bound(end_ptr, b.upperBound(End_Loop).product(b.upperBound(Node)));
 		
 		final TupleSet Next = f.noneOf(2);
 		for(Integer i = 0; i < scope - 1; i++){
@@ -142,30 +127,24 @@ public class PathIF {
 		final TupleSet Begins = f.noneOf(2);
 		Begins.add(f.tuple("Edge1", "Node1"));
 		Begins.add(f.tuple("Edge2", "Node2"));
-		Begins.add(f.tuple("Edge3", "Node2"));
+		Begins.add(f.tuple("Edge3", "Node3"));
 		Begins.add(f.tuple("Edge4", "Node3"));
-		Begins.add(f.tuple("Edge5", "Node4"));
-		Begins.add(f.tuple("Edge6", "Node5"));
-		Begins.add(f.tuple("Edge7", "Node5"));
 		b.boundExactly(begin , Begins);
 
 		final TupleSet Ends = f.noneOf(2);
 		Ends.add(f.tuple("Edge1", "Node2"));
 		Ends.add(f.tuple("Edge2", "Node3"));
 		Ends.add(f.tuple("Edge3", "Node4"));
-		Ends.add(f.tuple("Edge4", "Node5"));
-		Ends.add(f.tuple("Edge5", "Node5"));
-		Ends.add(f.tuple("Edge6", "Node2"));
-		Ends.add(f.tuple("Edge7", "Node6"));
+		Ends.add(f.tuple("Edge4", "Node2"));
 		b.boundExactly(end , Ends);
 
 		final TupleSet Node1 = f.noneOf(1);									/* Node1 */
 		Node1.add(f.tuple("Node1"));										/* Node1 */
 		b.boundExactly(Start , Node1);										/* Node1 */
 
-		final TupleSet Node6 = f.noneOf(1);									/* Node4 */
-		Node6.add(f.tuple("Node6"));										/* Node4 */
-		b.boundExactly(Finish , Node6);										/* Node4 */
+		final TupleSet Node4 = f.noneOf(1);									/* Node4 */
+		Node4.add(f.tuple("Node4"));										/* Node4 */
+		b.boundExactly(Finish , Node4);										/* Node4 */
 
 		return b;
 	}
@@ -174,7 +153,7 @@ public class PathIF {
 		try {
 			final PathIF model = new PathIF();							/* Path		Path */
 			final Solver solver = new Solver();
-			final Bounds b = model.bounds(10);
+			final Bounds b = model.bounds(3);
 			final Formula f = model.empty();
 			System.out.println(f);
 			solver.options().setSolver(SATFactory.DefaultSAT4J);
@@ -183,9 +162,9 @@ public class PathIF {
 			System.out.println(System.currentTimeMillis());
 			while(iterSols.hasNext()) {
 				final Solution s = (Solution) iterSols.next();
-				if(s.outcome() == Solution.Outcome.SATISFIABLE || s.outcome() == Solution.Outcome.TRIVIALLY_SATISFIABLE){
+				//if(s.outcome() == Solution.Outcome.SATISFIABLE || s.outcome() == Solution.Outcome.TRIVIALLY_SATISFIABLE){
 					System.out.println(s);	
-				}
+				//}
 			}
 
 		}	catch (NumberFormatException nfe) {}
